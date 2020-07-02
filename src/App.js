da899b81e58dc9d82a4aeb1563cd38937edfca53
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2020-07-02 16:39:21 +0200

    cambio 2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Irene en la segunda rama cambio 1
+					Irene en la segunda rama cambio 1 cambio 2
 				</a>
 			</header>
 		</div>
